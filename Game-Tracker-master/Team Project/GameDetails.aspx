338a2fdab4fdf9de1264728aa779c09f5b9e3fc7
gurpanthport <gurpanth1996@hotmail.com>
2016-06-15 23:34:19 -0400

    Comit to changes

---
+++ Game-Tracker-master/Team Project/GameDetails.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="GameDetails.aspx.cs" Inherits="Team_Project.GameDetails" %>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-1 col-md-5">
+                <h5>Add the Game Scores</h5>
+                <br />
+                <div class="form-group">
+                    <label class="control-label" for="teamATextBox">Department Name</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="teamATextBox" placeholder="Team A Name" required="true"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="teamBTextBox">Department Name</label>
+                   <asp:TextBox runat="server" CssClass="form-control" ID="teamBTextBox" placeholder="Team B Name" required="true"></asp:TextBox>
+                </div>
+                 <div class="form-group">
+                    
+                   <asp:TextBox runat="server" CssClass="form-control" ID="teamAScore" placeholder="Team A Score" required="true"></asp:TextBox>
+                </div>
+                 <div class="form-group">
+                    
+                   <asp:TextBox runat="server" CssClass="form-control" ID="teamBScore" placeholder="Score B Score" required="true"></asp:TextBox>
+                </div>
+                 <div class="form-group">
+                            
+                    
+                    <asp:Button Text="Cancel" ID="CancelButton" CssClass="btn btn-warning btn-lg" runat="server" 
+                        UseSubmitBehavior="false" CausesValidation="false" OnClick="CancelButton_Click" />
+                  <asp:Button Text="Save" ID="SaveButton" CssClass="btn btn-primary btn-lg" runat="server" OnClick="SaveButton_Click" />
+                 
+                
+               </div>
+                </div>
+                
+                <div class="col-md-offset-2 col-md-8">
+                <h1>Departments List</h1>
+                <a href="DepartmentDetails.aspx" class="btn btn-success btn-sm"><i class="fa fa-plus"></i> ADD Department</a>
+
+              
+
+               
+                  
+           
+</div>
+       
+        </div>
+        </div>
+    
+</asp:Content>
---
+++ Game-Tracker-master/Team Project/GameDetails.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+// using statements required for EF DB access
+using Team_Project.Models;
+using System.Web.ModelBinding;
+namespace Team_Project
+{
+    public partial class GameDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if ((!IsPostBack) && (Request.QueryString.Count > 0))
+            {
+                this.GetGame();
+            }
+        }
+
+        protected void GetGame()
+        {
+            // populate the form with existing department data from the db
+            int GameID = Convert.ToInt32(Request.QueryString["GameID"]);
+
+            // connect to the EF DB
+            using (GameData db = new GameData())
+            {
+                // populate a department instance with the departmentID from the URL parameter
+                Game updatedGame = (from Game in db.Games
+                                                where Game.gameID == GameID
+                                                select Game).FirstOrDefault();
+
+                // map the Department properties to the form controls
+                if (updatedGame != null)
+                {
+                    teamATextBox.Text = updatedGame.teamA;
+                    teamBTextBox.Text = updatedGame.TeamB;
+                }
+            }
+        }
+
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            // Redirect back to Students page
+            Response.Redirect("~/Default.aspx");
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            // Use EF to connect to the server
+            using (GameData db = new GameData())
+            {
+                // use the Department model to create a new Department object and
+                // save a new record
+                Game newGame = new Game();
+
+                int GameID = 0;
+
+                if (Request.QueryString.Count > 0)
+                {
+                    // get the id from url
+                    GameID = Convert.ToInt32(Request.QueryString["GameID"]);
+
+                    // get the current Department from EF DB
+                    newGame = (from Game in db.Games
+                                     where Game.gameID == GameID
+                                     select Game).FirstOrDefault();
+                }
+
+                // add form data to the new Department record
+                newGame.teamA = teamATextBox.Text;
+                newGame.TeamB = teamATextBox.Text;
+
+                //  Budget.Text =  updatedDepartment.Budget.ToString("{0:C}");
+
+                // use LINQ to ADO.NET to add / insert new Department into the database
+
+                // check to see if a new Department is being added
+                if (GameID == 0)
+                {
+                    db.Games.Add(newGame);
+                }
+
+                // save our changes - run an update
+                db.SaveChanges();
+
+                // Redirect back to the updated Department page
+                Response.Redirect("~/Default.aspx");
+            }
+        }
+    }
+}
